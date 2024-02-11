c1f7780601554c0bf9755ebae68f26af27eac776
ShadiBn <shadi.bonassif@gmail.com>
2024-02-11 15:49:24 +0200

    Created the Starting Point
    Added the Sections and Assets, imported and exported them in index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-export default function App() {
-  return (
-    <h1 className="text-5xl font-bold underline text-center my-10 p-2">
-      Hello world!
-    </h1>
-  )
-}
+import {Hero, PopularProducts, SuperQuality, Services, SpecialOffer,CustomerReviews, Subscribe, Footer } from "./sections/index.js"
+
+const App = () => (
+  <main className = 'relative'>
+      {/* <Nav/> */}
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className="padding">
+      <PopularProducts/>
+    </section>
+    <section className="padding">
+      <SuperQuality/>
+    </section>
+    <section className="padding padding-x py-10">
+      <Services/>
+    </section>
+    <section className="padding">
+      <SpecialOffer/>
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews/>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe/>
+    </section>
+    <section className="padding bg-black padding-x padding-t pb-8">
+      <Footer/>
+    </section>
+  </main>
+);
+
+export default App;
